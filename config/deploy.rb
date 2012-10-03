42a3e8b1cf4f26201f5fc634ca08bead095af387
Foy Savas <foy@sav.as>
2012-10-02 21:06:43 -0400

    ready deploy for assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 require 'bundler/capistrano'
 require 'rvm/capistrano'
+load "deploy/assets"
 
 set :branch, "master"
 
@@ -14,12 +15,20 @@ set :deploy_to, "/data/#{application}"
 set :repository, "git://github.com/userpedia/userpedia-website.git"
 set :scm, :git
 
+set :default_environment, { 'LANG' => 'en_US.UTF-8' }
+
 set :rvm_bin_path, "/usr/local/rvm/bin/"
 before 'deploy:setup', 'rvm:install_rvm'
+
 namespace :deploy do
   task :start do ; end
   task :end do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "god restart #{application}"
   end
+
+  task :link_db do
+    run "ln -fs #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+  end
+  before "deploy:assets:precompile", "deploy:link_db"
 end
